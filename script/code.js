8689844de806e5d98ee4f7cf1b95f8268df5af59
LeahBasson <leahbasson54@gmail.com>
2024-05-27 16:28:09 +0200

    Main Section

--- script/code.js
+++ script/code.js
@@ -1,1 +1,15 @@
-let total = document.querySelector('[]');
+let calculateTotal = document.querySelector('[calculateTotal]');
+
+document.querySelector('[calculateTotal]').addEventListener("click", ()=>{
+    let fQuantity = +document.querySelector('[quantityFsj]').value;
+    let mQuantity = +document.querySelector('[quantityMsj]').value;
+    
+    let firstTotal = 150.95 * fQuantity ;
+    document.querySelector('[displayFsj]').textContent = firstTotal.toFixed(2);
+
+    let secTotal = 180.95 * mQuantity;
+    document.querySelector('[displayMsj]').textContent = secTotal.toFixed(2);
+
+   finalTotal = firstTotal + secTotal;
+    document.querySelector('[finalTotal]').textContent = finalTotal.toFixed(2);
+});
